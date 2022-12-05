3d51424d5b0c8bb51e031130ba58b2750bc1d9fa
EFRobins <robinsfinley@gmail.com>
2022-12-05 01:44:40 +0000

    Init

---
+++ setupjogl232.bat
@@ -0,0 +1,2 @@
+set path=C:\Users\robin\Documents\Uni\Year3\jogl232\lib;%path%
+set classpath=.;C:\Users\robin\Documents\Uni\Year3\3DGraphics\jogl232\jar\jogl-all.jar;C:\Users\robin\Documents\Uni\Year3\3DGraphics\jogl232\jar\gluegen-rt.jar;%classpath%
